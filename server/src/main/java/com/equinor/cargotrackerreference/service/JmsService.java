6074de5484100abf54d66a31dccb5b35ef8029b3
Helge Håland <heh@equinor.com>
2020-03-02 13:33:08 +0100

    AB#8689 Fixing failing unittests due to JMS

--- server/src/main/java/com/equinor/cargotrackerreference/service/JmsService.java
+++ server/src/main/java/com/equinor/cargotrackerreference/service/JmsService.java
@@ -1,5 +1,6 @@
 package com.equinor.cargotrackerreference.service;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 import javax.annotation.Resource;
@@ -17,7 +18,9 @@ import javax.naming.NamingException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.stereotype.Service;
 
@@ -40,47 +43,59 @@ public class JmsService {
 	
 	@Resource(name = "jacksonJmsMessageConverter")
 	private MappingJackson2MessageConverter jacksonJmsMessageConverter;
+
+	@Autowired
+	private Environment env;
    	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 		
 	public void sendJmsMessage(Object message, String typeid, String operation) {
-		
-	       try {
-	    	   
-	    	   //Adding properties needed for the qpid library
-	   			Properties props = new Properties();
-	   			props.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.qpid.jms.jndi.JmsInitialContextFactory");
-	   			props.put("connectionfactory.myFactoryLookup", connectionUri);
-	   			props.put("topic.myTopicLookup", topic);
-	   		
-	   			Context context = new InitialContext(props);
+		/*
+		 * Disabling for Unittests
+		 */
+		if (Arrays.asList(env.getActiveProfiles()).contains("h2")) {
+			return;
+		}
 
-	   			ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("myFactoryLookup");
-	   			Connection connection = connectionFactory.createConnection(keyName, key);
-	   			connection.setExceptionListener(new ServiceBusExceptionListener());
-	   			connection.start();
-	   				   			
-	   			Topic serviceBusTopic = (Topic) context.lookup("myTopicLookup");
-	   			
-		        // Create sender-side Session and MessageProducer
-		        Session sendSession = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		        MessageProducer messageProducer = sendSession.createProducer(serviceBusTopic);		        		        	        
-		        		        
-		        Message jmsTextMessage = jacksonJmsMessageConverter.toMessage(message, sendSession);
-		        
-		        //Set an extra property telling what type of operation for the object
-		        jmsTextMessage.setStringProperty("operation", operation);
-				
-				logger.debug("Sending message to:\ntopic: {}\ntype: {}\noperation: {}\npayload: {}", messageProducer.getDestination(), jmsTextMessage.getStringProperty(AzureServiceBusConfiguration.TYPEID), operation, jmsTextMessage.getBody(String.class));
-		        
-				messageProducer.send(jmsTextMessage);
-				jmsTextMessage.acknowledge();
-				
-				sendSession.close();
-		        connection.close();		        		        
-	       } catch (JMSException e) {			
+		try {
+
+			// Adding properties needed for the qpid library
+			Properties props = new Properties();
+			props.put(Context.INITIAL_CONTEXT_FACTORY, "org.apache.qpid.jms.jndi.JmsInitialContextFactory");
+			props.put("connectionfactory.myFactoryLookup", connectionUri);
+			props.put("topic.myTopicLookup", topic);
+
+			Context context = new InitialContext(props);
+
+			ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("myFactoryLookup");
+			Connection connection = connectionFactory.createConnection(keyName, key);
+			connection.setExceptionListener(new ServiceBusExceptionListener());
+			connection.start();
+
+			Topic serviceBusTopic = (Topic) context.lookup("myTopicLookup");
+
+			// Create sender-side Session and MessageProducer
+			Session sendSession = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			MessageProducer messageProducer = sendSession.createProducer(serviceBusTopic);
+
+			Message jmsTextMessage = jacksonJmsMessageConverter.toMessage(message, sendSession);
+
+			// Set an extra property telling what type of operation for the object
+			jmsTextMessage.setStringProperty("operation", operation);
+
+			logger.debug("Sending message to:\ntopic: {}\ntype: {}\noperation: {}\npayload: {}",
+					messageProducer.getDestination(),
+					jmsTextMessage.getStringProperty(AzureServiceBusConfiguration.TYPEID), operation,
+					jmsTextMessage.getBody(String.class));
+
+			messageProducer.send(jmsTextMessage);
+			jmsTextMessage.acknowledge();
+
+			sendSession.close();
+			connection.close();
+		} catch (JMSException e) {
 			e.printStackTrace();
-			} catch (NamingException e) {
+		} catch (NamingException e) {
 			e.printStackTrace();
 		}
 	}
