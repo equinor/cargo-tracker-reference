0c24f41041ce64477db798863e5d5443240adc2f
Ronnie <rhenri@equinor.com>
2020-03-26 12:07:50 +0100

    Ignore 401 / 403 on static service

--- client/src/app/static.service.ts
+++ client/src/app/static.service.ts
@@ -1,10 +1,19 @@
 import { Inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { BASE_URL } from './tokens';
 import { Grade } from './shared/models/grade';
 import { Country, Terminal } from './shared/models/location';
 import { Region } from './shared/models/region';
 import { Company } from './shared/models/company';
+import { catchError } from 'rxjs/operators';
+import { of, throwError } from 'rxjs';
+
+const staticErrorHandler = catchError((err: HttpErrorResponse) => {
+  if (err.status === 401 || err.status === 403) {
+    return of([])
+  }
+  return throwError(err)
+})
 
 @Injectable({
   providedIn: 'root'
@@ -17,22 +26,27 @@ export class StaticService {
   }
 
   grades() {
-    return this.http.get<Grade[]>(`${this.baseUrl}/grade`);
+    return this.http.get<Grade[]>(`${this.baseUrl}/grade`)
+    .pipe(staticErrorHandler)
   }
 
   countries() {
-    return this.http.get<Country[]>(`${this.baseUrl}/country`);
+    return this.http.get<Country[]>(`${this.baseUrl}/country`)
+    .pipe(staticErrorHandler)
   }
 
   regions() {
-    return this.http.get<Region[]>(`${this.baseUrl}/region`);
+    return this.http.get<Region[]>(`${this.baseUrl}/region`)
+    .pipe(staticErrorHandler)
   }
 
   terminals() {
-    return this.http.get<Terminal[]>(`${this.baseUrl}/terminal`);
+    return this.http.get<Terminal[]>(`${this.baseUrl}/terminal`)
+    .pipe(staticErrorHandler)
   }
 
   companies() {
-    return this.http.get<Company[]>(`${this.baseUrl}/company`);
+    return this.http.get<Company[]>(`${this.baseUrl}/company`)
+    .pipe(staticErrorHandler)
   }
 }
