2e7ade70d602252ce0ad0640a1155fa59689fac9
Helge Håland (HEH) <HEH@equinor.com>
2020-03-31 15:06:01 +0200

    AB#7016 Improving logging in ct reference
    Now sending event to App Insight for all sending of data to CCT

--- server/src/main/java/com/equinor/cargotrackerreference/controller/BackendController.java
+++ server/src/main/java/com/equinor/cargotrackerreference/controller/BackendController.java
@@ -14,12 +14,14 @@ import com.equinor.cargotracker.common.domain.Grade;
 import com.equinor.cargotracker.common.domain.Region;
 import com.equinor.cargotracker.common.domain.Terminal;
 import com.equinor.cargotracker.common.domain.TradingArea;
+import com.equinor.cargotracker.common.utils.Constants;
 import com.equinor.cargotrackerreference.service.CompanyService;
 import com.equinor.cargotrackerreference.service.CountryService;
 import com.equinor.cargotrackerreference.service.GradeService;
 import com.equinor.cargotrackerreference.service.RegionService;
 import com.equinor.cargotrackerreference.service.TerminalService;
 import com.equinor.cargotrackerreference.service.TradingAreaService;
+import com.microsoft.applicationinsights.TelemetryClient;
 import com.microsoft.applicationinsights.core.dependencies.google.common.collect.Iterables;
 
 @RestController
@@ -29,57 +31,66 @@ public class BackendController {
 
 	@Autowired
 	private CompanyService companyService;
-	
+
 	@Autowired
 	private CountryService countryService;
-	
+
 	@Autowired
 	private GradeService gradeService;
-	
+
 	@Autowired
-	private RegionService regionService; 
-	
+	private RegionService regionService;
+
 	@Autowired
 	private TradingAreaService tradingAreaService;
-	
+
 	@Autowired
 	private TerminalService terminalService;
-	
+
+	@Autowired
+	private TelemetryClient telemetry;
+
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@RequestMapping(value = "/companies", method = RequestMethod.GET)
 	Company[] getAllCompanies() {
-		logger.debug("Getting all companies");
+		telemetry.trackEvent(Constants.CARGO_TRACKER_REFERENCE_SHORT + " sending companies to "
+				+ Constants.CRUDE_CARGO_TRACKER_SHORT);
 		return Iterables.toArray(companyService.getAllCompanies(), Company.class);
 	}
-	
+
 	@RequestMapping(value = "/countries", method = RequestMethod.GET)
 	Country[] getAllCountries() {
-		logger.debug("Getting all countries");
+		telemetry.trackEvent(Constants.CARGO_TRACKER_REFERENCE_SHORT + " sending countries to "
+				+ Constants.CRUDE_CARGO_TRACKER_SHORT);
 		return Iterables.toArray(countryService.getAllCountries(), Country.class);
 	}
-	
+
 	@RequestMapping(value = "/grades", method = RequestMethod.GET)
 	Grade[] getAllGrades() {
-		logger.debug("Getting all grades");
+		telemetry.trackEvent(
+				Constants.CARGO_TRACKER_REFERENCE_SHORT + " sending grades to " + Constants.CRUDE_CARGO_TRACKER_SHORT);
 		return Iterables.toArray(gradeService.getAllGrades(), Grade.class);
 	}
-	
+
 	@RequestMapping(value = "/regions", method = RequestMethod.GET)
 	Region[] getAllRegions() {
-		logger.debug("Getting all regions");
+		telemetry.trackEvent(
+				Constants.CARGO_TRACKER_REFERENCE_SHORT + " sending regions to " + Constants.CRUDE_CARGO_TRACKER_SHORT);
 		return Iterables.toArray(regionService.getRegions(), Region.class);
 	}
-	
+
 	@RequestMapping(value = "/tradingareas", method = RequestMethod.GET)
 	TradingArea[] getAllTradingAreas() {
-		logger.debug("Getting all tradingareas");
+		telemetry.trackEvent(Constants.CARGO_TRACKER_REFERENCE_SHORT + " sending trading areas to "
+				+ Constants.CRUDE_CARGO_TRACKER_SHORT);
 		return Iterables.toArray(tradingAreaService.getTradingAreas(), TradingArea.class);
 	}
 
 	@RequestMapping(value = "/terminals", method = RequestMethod.GET)
 	Terminal[] getAllTerminals() {
-		logger.debug("Getting all terminals");
+		telemetry.trackEvent(Constants.CARGO_TRACKER_REFERENCE_SHORT + " sending terminals to "
+				+ Constants.CRUDE_CARGO_TRACKER_SHORT);
 		return Iterables.toArray(terminalService.getTerminals(), Terminal.class);
 	}
 
